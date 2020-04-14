37ffdaa1082da9d283669741fc789a009519f5ad
Mel Maki <melody.rackham@gmail.com>
2020-04-14 16:42:54 -0600

    converts details.js into a class component using the construction function and super keyword

--- src/details.js
+++ src/details.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import Pet from '@frontendmasters/pet';
 
-const Details = (props) => {
-  return (
-    <pre>
-      {' '}
-      <code> {JSON.stringify(props, null, 4)} </code>
-    </pre>
-  );
-};
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+    };
+  }
+  omponentDidMount() {
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city} , ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.brees.primary,
+        loading: false,
+      });
+    });
+  }
+  render() {
+    return;
+  }
+}
 
 export default Details;
